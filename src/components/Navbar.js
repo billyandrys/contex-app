aaab9e8915afa988acfdb921536cf0ea4d611648
billyandryus <billyandrys@gmail.com>
2022-05-03 06:47:01 -0500

    Initialzing app useContex

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+
+    <nav className='navbar navbar-dark bg-dark'>
+      <span className='navbar-brand'>
+        <h2>Hello Luis</h2>
+      </span>
+      <button>Sign out</button>
+    </nav>
+  )
+}
